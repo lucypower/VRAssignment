54b7a9329b69a8eee6dc521a8fd46d4870cc282b
lucypower <luciellepenguin9@gmail.com>
2023-05-04 13:44:34 +0100

    May 4

--- Assets/Scripts/BarrierReflect.cs
+++ Assets/Scripts/BarrierReflect.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class BarrierReflect : MonoBehaviour
 {
-    private Rigidbody m_rb;
-    Vector3 m_velocity;
-
-    private void Awake()
-    {
-        m_rb = GetComponent<Rigidbody>();
-    }
-
-    private void Update()
-    {
-        m_velocity = m_rb.velocity;
-    }
+    [SerializeField] private float m_bounce;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
-            var speed = m_velocity.magnitude;
-            var direction = Vector3.Reflect(m_velocity.normalized, collision.contacts[0].normal);
+            Rigidbody ballRB = collision.rigidbody;
 
-            m_velocity = direction * Mathf.Max(speed, 10f);
+            ballRB.AddExplosionForce(m_bounce, collision.contacts[0].point, 2);
         }
     }
 }
